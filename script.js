88f6df25bb51fe5ae36581282d3fbc10603c10bb
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-10-05 19:00:16 +0800

    implement win condition logic

--- script.js
+++ script.js
@@ -25,19 +25,24 @@ const drawBoard = (function (){
 const gameContoller = (function (){
 let isPlayer1 = true;
 const player1 = createPlayer();
+player1.symbol= "X";
 const player2= createPlayer();
+player2.symbol="O";
 let moves =0;
 
 console.log(gameboard);
 
-while(moves < 2){ //9
+while(moves < 6){ //9
  let currentPlayer = isPlayer1? player1 : player2;
  let choice = prompt(currentPlayer.name + " please choose position (row,col)");
  let input = choice.split(",");
 gameboard[input[0]][input[1]] = currentPlayer.symbol;
+checkWinner(currentPlayer);
+
 
 isPlayer1 = !isPlayer1;
 moves++;
+
 }
 
 console.log(gameboard);
@@ -45,19 +50,50 @@ console.log(gameboard);
 })();
 
 
-function Player(name, symbol) {
+function Player(name) {
     this.name = name;
-    this.symbol =symbol;
+    this.symbol;
   }
 
 
 function createPlayer(){
     playerName = prompt("Enter player's name");
-    playerSymbol = prompt("Enter " + playerName+ "'s symbol");
-
-    return new Player(playerName, playerSymbol);
+    return new Player(playerName);
 }
 
-function checkWinner(){
+function checkWinner(currentPlayer){
+    symbol = currentPlayer.symbol;
+    let name = currentPlayer.name;
 
+    //Win condition
+    //horizontal
+if(gameboard[0][0] == symbol && gameboard[0][1] == symbol && gameboard[0][2] == symbol){
+    alert(name + " wins!");
 }
+if(gameboard[1][0] == symbol && gameboard[1][1] == symbol && gameboard[1][2] == symbol){
+    alert(name + " wins!");
+}
+if(gameboard[2][0] == symbol && gameboard[2][1] == symbol && gameboard[2][2] == symbol){
+    alert(name + " wins!");
+}
+
+    //vertical
+if(gameboard[0][0] == symbol && gameboard[1][0] == symbol && gameboard[2][0] == symbol){
+    alert(name + " wins!");
+}
+if(gameboard[0][1] == symbol && gameboard[1][1] == symbol && gameboard[2][1] == symbol){
+    alert(name + " wins!");
+}
+if(gameboard[0][2] == symbol && gameboard[1][2] == symbol && gameboard[2][2] == symbol){
+    alert(name + " wins!");
+}
+
+ //diagonal
+ if(gameboard[0][0] == symbol && gameboard[1][1] == symbol && gameboard[2][2] == symbol){
+    alert(name + " wins!");
+}
+if(gameboard[0][2] == symbol && gameboard[1][1] == symbol && gameboard[2][0] == symbol){
+    alert(name + " wins!");
+}
+
+} 
