290600565319702c634546ae82c314dd74999c97
Aaron Richardson <aaronrichardsonsg@gmail.com>
2024-10-08 23:52:50 +0800

    add player creation input boxes

--- script.js
+++ script.js
@@ -1,74 +1,54 @@
-// 3x3 grid gameboard - where the game will be played
-const gameboard = (function (){
-const rows =3;
-const colums =3;
-const board = [];
+//TODO
+// RESET BUTTON
+// document that keeps score of game
 
-for(let i =0; i<rows; i++){
-    board[i]=[];
-    for(let j =0; j<colums; j++){
-        board[i][j]= i +"" +j;
-    }
-}
+cells = document.querySelectorAll(".cell");
+const startButton = document.querySelector('#start');
 
-return board;
-})();
+startButton.addEventListener("click", function(){
+startGame();
+});
 
-
+function startGame(){    
+    const player1Name = document.querySelector('#player1').value;
+    const player2Name = document.querySelector('#player2').value;
+     const  p1 = createPlayer(player1Name, "X");
+     const p2 = createPlayer(player2Name, "O");
+     gameController(p1, p2);
+   
+  }
 
 //game controller
-const gameContoller = (function (){
+function gameController(player1, player2){
 let isPlayer1 = true;
-const player1 = createPlayer();
-player1.symbol= "X";
-const player2= createPlayer();
-player2.symbol="O";
 let moves =0;
-
-console.log(gameboard);
-cells = document.querySelectorAll(".cell");
-
-
-
-
- 
-// let choice = prompt(currentPlayer.name + " please choose position (row,col)");
- //let input = choice.split(",");
-//gameboard[input[0]][input[1]] = currentPlayer.symbol;
-//checkWinner(currentPlayer);
-
 
 cells.forEach(cell => {
     cell.addEventListener("click", function(){
         let currentPlayer = isPlayer1? player1 : player2;
         cell.textContent=currentPlayer.symbol;
+        cell.classList.add('clicked'); // add class to make button disabled. 'cell.disable = true' was greying out button text 
         checkWinner(currentPlayer);
 
         isPlayer1 = !isPlayer1;
         moves++;
-       
         console.log(moves);
 
     });
 });
 
+};
 
 
-
-//console.log(gameboard);
-
-})();
-
-
-function Player(name) {
+function Player(name, symbol) {
     this.name = name;
-    this.symbol;
+    this.symbol = symbol;
   }
 
 
-function createPlayer(){
-    playerName = prompt("Enter player's name");
-    return new Player(playerName);
+function createPlayer(name, symbol){
+
+    return new Player(name, symbol);
 }
 
 
@@ -92,59 +72,23 @@ function createPlayer(){
             ["cell-02", "cell-11", "cell-20"]
         ];
 
+
+        // iterate through winningCombination arrays to see if win condition has been met on buttons
         for(x of winningCombinations){
             if(
                 document.getElementById(x[0]).textContent==symbol &&
                 document.getElementById(x[1]).textContent==symbol &&
                 document.getElementById(x[2]).textContent==symbol
             ){
+                // but in text to DOM
                 alert(name + " wins!")
+                cells.forEach(cell => {
+                    cell.disabled=true;
+                });
+
             }
         }
 
 
     }
 
-  
-
-
-/*
-
-function checkWinner(currentPlayer){
-    symbol = currentPlayer.symbol;
-    let name = currentPlayer.name;
-
-    //Win condition
-    //horizontal
-if(gameboard[0][0] == symbol && gameboard[0][1] == symbol && gameboard[0][2] == symbol){
-    alert(name + " wins!");
-}
-if(gameboard[1][0] == symbol && gameboard[1][1] == symbol && gameboard[1][2] == symbol){
-    alert(name + " wins!");
-}
-if(gameboard[2][0] == symbol && gameboard[2][1] == symbol && gameboard[2][2] == symbol){
-    alert(name + " wins!");
-}
-
-    //vertical
-if(gameboard[0][0] == symbol && gameboard[1][0] == symbol && gameboard[2][0] == symbol){
-    alert(name + " wins!");
-}
-if(gameboard[0][1] == symbol && gameboard[1][1] == symbol && gameboard[2][1] == symbol){
-    alert(name + " wins!");
-}
-if(gameboard[0][2] == symbol && gameboard[1][2] == symbol && gameboard[2][2] == symbol){
-    alert(name + " wins!");
-}
-
- //diagonal
- if(gameboard[0][0] == symbol && gameboard[1][1] == symbol && gameboard[2][2] == symbol){
-    alert(name + " wins!");
-}
-if(gameboard[0][2] == symbol && gameboard[1][1] == symbol && gameboard[2][0] == symbol){
-    alert(name + " wins!");
-}
-
-} 
-
-*/
